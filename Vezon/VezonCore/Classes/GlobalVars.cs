306043e7df159670b02a55fe7a11c3ed5258b807
Bitl <bitlblox@gmail.com>
2024-10-24 12:34:56 -0700

    oop

--- Vezon/VezonCore/Classes/GlobalVars.cs
+++ Vezon/VezonCore/Classes/GlobalVars.cs
@@ -6,7 +6,7 @@ public static class _G
     public static readonly string ProjectName = "Inertia Engine";
     public static readonly string ProjectShortName = "Inertia";
     public static readonly Vezon VezonInstance = new Vezon();
-    public static Language? CurLanguage = new Language("English");
+    public static Language? CurLanguage = new Language();
 
     public static string LoadTranslatedString(string untranslated)
     {
