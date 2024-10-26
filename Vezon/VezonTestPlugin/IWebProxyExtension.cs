31f451ca83ea72f5a132c36e1cea8fb6dbd2e2bc
Bitl <bitlblox@gmail.com>
2024-10-26 12:13:05 -0700

    organize extension

---
+++ Vezon/VezonTestPlugin/IWebProxyExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy.EventArguments;
+
+namespace VezonWebProxy
+{
+    public class IWebProxyExtension
+    {
+        public virtual void OnProxyStart() { }
+        public virtual void OnProxyStopped() { }
+
+        public virtual bool IsValidURL(string absolutePath, string host) { return false; }
+
+        public virtual Task OnBeforeTunnelConnectRequest(object sender, TunnelConnectSessionEventArgs e) { return Task.FromResult(0); }
+        public virtual async Task OnRequest(object sender, SessionEventArgs e)
+        {
+            string query = e.HttpClient.Request.RequestUri.Query;
+            e.Ok($"Response to '{query}':\nTest successful.");
+        }
+    }
+}
--- Vezon/VezonTestPlugin/WebProxy.cs
+++ Vezon/VezonTestPlugin/WebProxy.cs
@@ -14,21 +14,6 @@ using System.Reflection;
 
 namespace VezonWebProxy
 {
-    public class IWebProxyExtension
-    {
-        public virtual void OnProxyStart() { }
-        public virtual void OnProxyStopped() { }
-
-        public virtual bool IsValidURL(string absolutePath, string host) { return false; }
-
-        public virtual Task OnBeforeTunnelConnectRequest(object sender, TunnelConnectSessionEventArgs e) { return Task.FromResult(0); }
-        public virtual async Task OnRequest(object sender, SessionEventArgs e)
-        {
-            string query = e.HttpClient.Request.RequestUri.Query;
-            e.Ok($"Response to '{query}':\nTest successful.");
-        }
-    }
-
     public class WebProxy
     {
         public IVezonExtension? Parent = null;
